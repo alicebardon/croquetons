d795944fec22cd3afc94956bdc274b05f3785005
Alice Bardon <abardon.catineau@gmail.com>
2022-11-15 23:25:43 +0100

    creating about page and link to it in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "home", to: "pages#home"
   root to: "pages#home"
+  get "/about", to: "pages#about"
   # Defines the root path route ("/")
   # root "articles#index"
 end
