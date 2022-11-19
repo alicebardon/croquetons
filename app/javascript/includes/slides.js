9819f499813f689111aebdb79a12ee4e4141280f
Alice Bardon <abardon.catineau@gmail.com>
2022-11-19 23:29:20 +0100

    Trying JS for swipping

--- app/javascript/includes/slides.js
+++ app/javascript/includes/slides.js
@@ -1,9 +1,38 @@
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+  var slides = document.querySelectorAll('#slides .slide');
+  var currentSlide = 0;
+  var activeSlide = document.querySelector('#slides .slide:first-child');
+  console.log(activeSlide);
 
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+  // activeSlide.classList.add("red");
+
+    activeSlide.classList.add("showing");
+
+
+  const dislike = document.getElementById("dislike");
+  console.log(dislike);
+
+  dislike.addEventListener("click", (event) => {
+    goToSlide('dislike');
+  });
+
+  const like = document.getElementById("like");
+  like.addEventListener("click", (event) => {
+    goToSlide('like');
+  });
+
+
+function goToSlide(action) {
+  activeSlide.classList.remove("showing");
+  console.log(activeSlide);
+  activeSlide = activeSlide.nextElementSibling(".slide");
+
+  if (action == "like") {
+  } else {
+
+  }
+
+  activeSlide.classList.add("showing");
+  // slides[currentSlide].className = 'slide';
+  // currentSlide = (currentSlide+1)%slides.length;
+  // slides[currentSlide].className = 'slide showing';
 }
